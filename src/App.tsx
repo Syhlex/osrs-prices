134f35397ab9bbbad6c048365d3a61e6a3e142e1
Ian Zhang <ianzh4ng@gmail.com>
2022-12-03 15:38:16 -0800

    Add home page markup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Home } from 'pages/home';
+
+export const App = () => {
+  return (
+    <>
+      <header></header>
+      <Home />
+      <footer></footer>
+    </>
+  );
+};
