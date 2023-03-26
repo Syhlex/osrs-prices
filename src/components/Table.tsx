e036cef15216ea64d1acbeb12c093773a10fc60c
Ian Zhang <ianzh4ng@gmail.com>
2023-03-26 15:59:53 -0700

    Set table columns and height to fit

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { AgGridReact, AgGridReactProps } from 'ag-grid-react';
 import cns from 'classnames';
 
 import 'ag-grid-community/dist/styles/ag-grid.css'; // Core grid CSS, always needed
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css'; // Optional theme CSS
+import { GridOptions, GridReadyEvent } from 'ag-grid-community';
 
 export interface TableProps extends AgGridReactProps {
   styleProps?: { container?: string };
 }
 
+const defaultGridOptions: GridOptions = {
+  domLayout: 'autoHeight',
+};
+
 export const Table = ({ styleProps, ...agGridReactProps }: TableProps) => {
+  const onGridReady = useCallback((e: GridReadyEvent) => {
+    e.api.sizeColumnsToFit();
+
+    if (agGridReactProps.onGridReady) {
+      agGridReactProps.onGridReady(e);
+    }
+  }, []);
+
   return (
     <div className={cns(styleProps?.container, 'ag-theme-alpine')}>
-      <AgGridReact {...agGridReactProps}></AgGridReact>
+      <AgGridReact
+        {...defaultGridOptions}
+        {...agGridReactProps}
+        onGridReady={onGridReady}
+      ></AgGridReact>
     </div>
   );
 };
