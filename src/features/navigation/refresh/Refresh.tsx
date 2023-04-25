984cb51e011cdd6f04495a5f0ab02c2a2ddf2b3e
Ian Zhang <ianzh4ng@gmail.com>
2023-04-24 21:33:52 -0700

    Implement auto refresh

--- src/features/navigation/refresh/Refresh.tsx
+++ src/features/navigation/refresh/Refresh.tsx
@@ -1,22 +1,41 @@
 import { ItemsContext } from 'context/Items/ItemsContext';
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+
+const AUTO_REFRESH_INTERVAL = 60;
 
 export const Refresh = () => {
   const itemsApi = useContext(ItemsContext).api;
-  const timeRemaining = 0;
+  const [timeRemaining, setTimeRemaining] = useState(AUTO_REFRESH_INTERVAL);
 
-  const onRefresh = () => {
+  const refreshData = () => {
     if (itemsApi) {
       itemsApi.refreshData();
+      setTimeRemaining(60);
     }
   };
+
+  useEffect(() => {
+    const intervalId = window.setInterval(() => {
+      setTimeRemaining((prevTime) => prevTime - 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (timeRemaining === 0) {
+      refreshData();
+    }
+  }, [timeRemaining]);
 
   return (
     <div>
       <input type="checkbox" />
       Auto-refresh
       {timeRemaining}
-      <button onClick={onRefresh}>Refresh</button>
+      <button onClick={refreshData}>Refresh</button>
     </div>
   );
 };
