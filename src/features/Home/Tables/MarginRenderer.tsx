3b43140c96bb03463019cd15a5bc38ef3fe1fd24
Mena Wang <wang.mena@gmail.com>
2023-05-08 23:56:57 -0700

    spacing

--- src/features/Home/Tables/MarginRenderer.tsx
+++ src/features/Home/Tables/MarginRenderer.tsx
@@ -5,7 +5,7 @@ import styles from './HomeTables.mod.scss';
 export const MarginRenderer: React.FC<ICellRendererParams> = ({value}) => {
     return (
         <div className={value >= 0 ? styles["margin-positive"] : styles["margin-negative"]}>
-        {value}
-      </div>
+            {value}
+        </div>
     );
   };
