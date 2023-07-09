84236e7ffdb3e1876d17a2b6cfeec7b7996c2878
Ian Zhang <ianzh4ng@gmail.com>
2023-07-08 17:40:10 -0700

    Add initial ItemDetails setup

--- src/features/ItemDetails/ItemDetails.tsx
+++ src/features/ItemDetails/ItemDetails.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button, Icon, IconName } from 'components';
 
 export interface ItemDetails {
   id: string;
@@ -6,5 +7,35 @@ export interface ItemDetails {
 }
 
 export const ItemDetails = (props: ItemDetails) => {
-  return <div>{`${props.name} (Item ID: ${props.id})`}</div>;
+  return (
+    <div>
+      {`${props.name} (Item ID: ${props.id})`}
+      <Button
+        variant="nav"
+        onClick={() => {
+          window.open(
+            `https://oldschool.runescape.wiki/w/Special:Lookup?type=item&id=${props.id}`,
+          );
+        }}
+      >
+        Wiki
+      </Button>
+      <Button
+        variant="nav"
+        onClick={() => {
+          window.open(
+            `https://secure.runescape.com/m=itemdb_oldschool/viewitem?obj=${props.id}`,
+          );
+        }}
+      >
+        GEDB
+      </Button>
+      <Button variant="nav">
+        <Icon name={IconName.Copy} />
+      </Button>
+      <Button variant="nav">
+        <Icon name={IconName.Heart} />
+      </Button>
+    </div>
+  );
 };
--- src/features/ItemDetails/ItemDetailsContainer.tsx
+++ src/features/ItemDetails/ItemDetailsContainer.tsx
@@ -7,8 +7,9 @@ import { ItemDetails } from './ItemDetails';
 export const ItemDetailsContainer = () => {
   const { id = '' } = useParams();
   const { itemsMap } = useItems();
-  const itemName = itemsMap[id]?.name ?? '';
-  useTitle(itemName);
+  const item = itemsMap[id];
+  const itemName = item?.name ?? '';
+  useTitle(item?.name ?? '');
 
   return <ItemDetails id={id} name={itemName} />;
 };
